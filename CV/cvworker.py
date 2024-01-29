1cdaaaa8da8dae50268a355a6fbe28140dc966d7
micraow <15190450708@163.com>
2024-01-29 16:47:06 +0800

    划词翻译 重构cvworker

--- CV/cvworker.py
+++ CV/cvworker.py
@@ -1,20 +1,28 @@
 # 以下内容为接口规范，务必实现，除此之外还可另外创建类或方法，只要要求的接口能正常工作
 # 建议其他方法在另外的文件里写再import进来，保持有序
+import cv2
+
+
 class cv():
     def __init__(self):
-        print("初始化的工作（如果有）")
+        self.cap = cv2.VideoCapture(0)  # 参数为0时调用本地摄像头；参数为1时调用外接摄像头
+        self.index = 0
+        # print("初始化的工作（如果有）")
 
     def takePic(self):
-        path = "/path/to/image"
+        ret, frame = self.cap.read()  # ret(bool)有无读取到图片
+        cv2.imwrite("/tmp/prism"+str(self.index)+".jpg", frame)
+        path = "/tmp/prism"+str(self.index)+".jpg"
+        index = index + 1
         return path
 
-    def startCapture(self, wait_time):
-        # 间隔固定时间（wait_time)(毫秒) 拍照，获得一系列图片，用于合成,直到stopCapture被调用
-        pass
+    # def startCapture(self, wait_time):
+    #     # 间隔固定时间（wait_time)(毫秒) 拍照，获得一系列图片，用于合成,直到stopCapture被调用
+        
 
-    def stopCapture(self):
-        images_paths=[]
-        return images_paths
+    # def stopCapture(self):
+    #     images_paths = []
+    #     return images_paths
 
     def stitch(self, images_paths):
         # images_paths是一系列要拼接的图片的路径构成的列表
--- app/translate.py
+++ app/translate.py
@@ -2,8 +2,9 @@ from rec.paddleocr import recognize
 from CV import cvworker
 from PySide6.QtCore import QObject, Signal, Slot
 import sys
-from translate import bing_dict,bing,deepl,offline,youdao
+from translate import bing_dict, bing, deepl, offline, youdao
 import network
+from time import sleep
 sys.path.append("./CV")
 sys.path.append("./rec")
 
@@ -13,34 +14,50 @@ class translator(QObject):
         super().__init__(self)
         self.worker = cvworker.cv()
         self.img2txt = recognize.img2txt()
+        self.photoFlag = False
 
-    def txt2txt(self,content):
+    def txt2txt(self, content: str):
         """调用translate的接口,实现英译中
 
         Args:
             content (str): 翻译内容
-        
+
         Returns:
             dict: {后端名称:结果}
 
         """
-        result={}
-        if network.getNetworkstatus()==True:
-            for backend in [bing,deepl,youdao,offline]:
-                res=backend.translate(content)
-                result[backend.getName()]=res
-        
+        content = content.strip()
+        result = {}
+        if network.getNetworkstatus() == True:
+            if content.isalpha() != True:
+                for backend in [bing, deepl, youdao, offline]:
+                    res = backend.translate(content)
+                    result[backend.getName()] = res
+            else:
+                backend = bing_dict
+                res = backend.explain(content)
+                result[backend.getName()] = res
         else:
-            backend=offline
-            res=backend.translate(content)
-            result[backend.getName()]=res
-        
-        return result
+            backend = offline
+            res = backend.translate(content)
+            result[backend.getName()] = res
 
-        
+        return result
 
     def photoTranslate(self):
         path = self.worker.takePic()
-        res=self.img2txt.rec(path)
-        string="".join(res)
+        res = self.img2txt.rec(path)
+        string = "".join(res)
+        return self.txt2txt(string)
+
+    def liveTranslate(self):
+        # self.worker.startCapture(100) # 100ms一张
+        images_path = []
+        while self.photoFlag:
+            path = self.worker.takePic()
+            images_path.append(path)
+            sleep(0.1)  # 单位是秒
+        path = self.worker.stitch(images_path)
+        res = self.img2txt.rec(path)
+        string = "".join(res)
         return self.txt2txt(string)
