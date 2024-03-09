e5de1ddb3318b10075fdde3984294852f6ba53c9
micraow <15190450708@163.com>
2024-03-09 17:02:47 +0800

    vue样板代码

---
+++ web/front/prism-web/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '../components/WelcomePage.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
---
+++ web/front/prism-web/vite.config.js
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  minify: true, // 是否压缩代码
+  sourceMap: true, // 是否生成sourceMap
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
