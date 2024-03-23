deff46a6da6ee3ac773c04fc5d37a2d8598e8060
锦芸Offical <13434019+shengjls@user.noreply.gitee.com>
2024-03-23 22:21:18 +0800

    web/front/prism-web/src/views/InternetConnection.vue

---
+++ web/front/prism-web/src/views/InternetConnection.vue
@@ -0,0 +1,9 @@
+<template>
+    <main>
+      <a-page-header
+        style="border: 1px solid rgb(235, 237, 240)"
+        title="网络连接"
+        sub-title="请连接至Internet"
+      />
+    </main>
+</template>
