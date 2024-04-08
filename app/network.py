33e0591c559d39d646167c2fe8bbac8784a2a7ba
peterzhao23 <160157121+peterzhao23@users.noreply.github.com>
2024-04-08 23:24:21 +0800

    只是粗略写了一下network，没有套用flask框架

--- app/network.py
+++ app/network.py
@@ -1,5 +1,6 @@
 import requests
-
+import pywifi
+import json
 
 def getNetworkstatus():
     '''
@@ -10,3 +11,17 @@ def getNetworkstatus():
         return True
     except:
         return False
+def scanwifi():
+    wifi=pywifi.PyWiFi()
+    iface=wifi.interface()[0]
+    iface.scan()
+    results=iface.scan_result()
+    ssid=results.ssid.encode("raw_unicode_escape").decode("utf-8")
+    a={"ssid":ssid}
+    a.dumps()
+    return a
+
+    
+
+    
+
