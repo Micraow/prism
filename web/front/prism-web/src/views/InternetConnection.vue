c901ced6c388b743d704deda204404e55d4871b0
jinyunCN <13434019+shengjls@user.noreply.gitee.com>
2024-04-13 21:52:21 +0800

    1

--- web/front/prism-web/src/views/InternetConnection.vue
+++ web/front/prism-web/src/views/InternetConnection.vue
@@ -1,9 +1,7 @@
 <template>
-    <main>
-      <a-page-header
-        style="border: 1px solid rgb(235, 237, 240)"
-        title="网络连接"
-        sub-title="请连接至局域网"
-      />
-    </main>
+  <main>
+    <a-space direction="vertical">
+      <a-page-header style="border: 1px solid rgb(235, 237, 240)" title="网络连接" sub-title="请连接至局域网" />
+    </a-space>
+  </main>
 </template>
