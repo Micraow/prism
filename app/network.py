455262d7e4bf7283ce8ba9004b599df37a04e3dc
micraow <15190450708@163.com>
2024-04-13 14:45:39 +0800

    网络层完成

--- app/network.py
+++ app/network.py
@@ -1,5 +1,6 @@
-import requests
 import time
+import requests
+import os
 
 
 def getNetworkstatus():
@@ -18,18 +19,44 @@ def scan_network():
     扫描附近局域网，返回SSID和是否需要密码
     '''
     # pywifi无法获取是否需要密码，那也是个过时的库了
-    # 调用linux下的networkmanager来做这块
+    # 调用linux下的networkmanager(nmcli)来做这块
     # 目前的思路是对nmcli的返回结果进行字符串解析
-    return {}  # TODO
+    os.system('nmcli radio wifi on')  # 打开wifi设备
+    output = os.popen('nmcli dev wifi list').read()
+    output = output.split('\n')
+    output = output[1:]
+    result = {}
+    output.pop()
+    for line in output:
+        line = line.split()
+        if line.pop() == 'WPA1' or 'WPA2':
+            need_password = True
+        else:
+            need_password = False
+
+        if line[0] == '*':
+            ssid = line[2]
+        else:
+            ssid = line[1]
+        result[ssid] = {'ssid': ssid, 'need_password': need_password}
+    print(result)
+
+    return result
 
 
 def connect_wifi(ssid: str, password: str):
     '''
     传入SSID和密码尝试连接，即使这个网络没有密码，也收到一个空字符串
     '''
-    # TODO 你调用PyWiFi来连接
-    state = True
-    return state
+    if password != '':
+        os.system('nmcli dev wifi connect '+ssid+' password \"'+password+'\"')
+
+    else:
+        os.system('nmcli dev wifi connect '+ssid)
+
+    time.sleep(5)
+
+    return getNetworkstatus()
 
 
 if __name__ == "__main__":
