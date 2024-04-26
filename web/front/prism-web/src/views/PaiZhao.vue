adac834be1c5d9786b8313630acecfcaff3a8547
jinyunCN <13434019+shengjls@user.noreply.gitee.com>
2024-04-26 21:41:20 +0800

    adds

--- web/front/prism-web/src/views/PaiZhao.vue
+++ web/front/prism-web/src/views/PaiZhao.vue
@@ -2,8 +2,17 @@
   <main>
     <a-space direction="vertical" style="width: 100%;">
       <a-page-header style="border: 1px solid rgb(235, 237, 240)" title="拍照翻译" sub-title="让我做你的眼睛" />
-    
-      <TurnToHistoryPaiZhao />
+
+      <a-card>
+        <p style="text-align: center; font-size: large; font-weight: bold">点击下方按钮即可拍照翻译</p>
+        <p style="text-align: center">
+          <a-button type="primary" shape="circle" size="large" style="height: 120px; width: 120px">
+            <template #icon>
+              <CameraOutlined style="fontsize: 36px" />
+            </template>
+          </a-button>
+        </p>
+      </a-card>
 
       <a-collapse v-model:activeKey="activeKey" :bordered="false">
         <a-collapse-panel key="1" header="必应翻译">
@@ -19,6 +28,8 @@
           <p>{{ lixian }}</p>
         </a-collapse-panel>
       </a-collapse>
+
+      <TurnToHistoryPaiZhao />
 
       <a-card>
         <p style="text-align: center;font-size: large;font-weight: bold;">历史记录</p>
