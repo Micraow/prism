c19d94b68a347facba03fcf749111b132adf4149
micraow <15190450708@163.com>
2025-01-04 20:02:04 +0800

    发布web branch

--- app/main.py
+++ app/main.py
@@ -1,16 +1,2 @@
-from pathlib import Path
-from PySide6.QtQml import QQmlApplicationEngine, qmlRegisterType
-from PySide6.QtCore import QUrl
-from PySide6.QtGui import QGuiApplication
-import sys
-import backend
-qml_file = Path(__file__).parent/"QML/main.qml"
-app = QGuiApplication(sys.argv)
-view = QQmlApplicationEngine()
-# translator=backend.translator()
-qmlRegisterType(backend.Translator, 'Translator', 1, 0, 'Translator')
-
-view.load(QUrl.fromLocalFile(qml_file.resolve()))
-del view
-sys.exit(app.exec())
-
+# 启动Flask
+pass
--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ pip install pip -U
 pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
 pip install "paddleocr>=2.0.1"
 pip install -r requirements.txt
-curl -o model.temp https://alist.pengs.top/d/64816595359b1e5db47f442f901a98ce4efd8481/alist/models/Helsinki-NLP/opus-mt-en-zh/pytorch_model.bin
+curl -o model.temp https://hf-mirror.com/Helsinki-NLP/opus-mt-en-zh/resolve/main/pytorch_model.bin?download=true
 mv model.temp translate/hf_model/pytorch_model.bin
 
 
